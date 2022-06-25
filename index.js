258884d62d7cdaac8786083f09fb6b2b417a2b38
HannahEbb <hannah.ebb.he@gmail.com>
2022-06-25 19:16:44 -0300

    asyc-await-ok

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const client = MongoClient(process.env.URL_CONNECT_MONGO);
 let db;
 
 client.connect().then(() => {
-    db = client.db('bate_papo_uol'); //bate_papo_uol é um banco de dados que criei, que vai conter coleções
+    db = client.db('bate_papo_uol'); //bate_papo_uol é o banco de dados que criei, que vai conter coleções
+}).catch((error) => {
+    console.log(`Erro ao conectar o banco de dados: ${error}`);
 });
 
 const app = express();
@@ -21,36 +23,61 @@ app.use(cors());
 app.use(json());
 
 
-app.post('/participants', (req, res) => {
+app.post('/participants', async (req, res) => {
     const { name } = req.body; 
+    const jaCadastrado = await db.colllection("participants").findOne({ name: name });
 
         if( !name ) {  //validar nome com lib joi
             res.status(422).send('Preencha seu nome, por favor!');
             return; //'return forçado' para nao executar o resto do codigo!
-        } else if() {
+        } else if(jaCadastrado) {
             //se o nome ja estivar cadastrado no mongo, retornar status erro 409! > AULA SEG 20/06 1H39MIN - 1H55MIN
             res.status(409).send('Opa! Este nome já está cadastrado.');
             return;
         };
 
-    db.collection("participants")
-    .insertOne( { name: name, lastStatus: Date.now()} )
-    .then(() => {
-       let time = dayjs.format("HH:mm:ss");
-        db.collection("messages")
-        .insertOne( {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time })
-        .then(() => {
-            res.status(201).send('Usuario entrou na sala com sucesso!');
-        }) // caso de sucesso salvar a msg de entrada
+    await db.collection("participants").insertOne( { name: name, lastStatus: Date.now()} );
+   
+    let time = dayjs.format("HH:mm:ss"); // ou new Date().getTime() ??
+
+    await db.collection("messages").insertOne( { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time });
+        
+    res.status(201).send('Usuario entrou na sala com sucesso!');
+       // caso de sucesso salvar a msg de entrada
     });//salvar participante no mongodb (formado especificado)
-});
 
 
 app.get('/participants', (req, res) => {
-   db.collection('participants').find().toArray().then(participants => {
-    res.send(participants);
+    try {
+        const arrayParticipantes = await db.collection('participants').find().toArray();
+        res.send(arrayParticipantes);
+    } catch(error) {
+        console.log(error);
+        res.status(500).send('Nao foi possivel pegar o array com todos os participantes');
+    }
+  
    }); //retonna a lista de todos os participantes
-});
+
+setInterval(
+function deletarAutomatico () {
+    app.delete('/participants', async (req, res) => {
+        
+    try {
+        // const names = db.collection("participants").findMany([{ lastStatus: 11000 }]).name ?? > array de nomes
+        // como pegar o(s) nome(s) de dento do documento com uma condicao?
+        // testar no mongo.
+        await db.collection("participants").deleteMany( [{ lastStatus: 11000 }] );
+   
+        let time = dayjs.format("HH:mm:ss"); // ou new Date().getTime() ??
+        // faz um map em names executanto o codigo abaixo para cada 'name'do array:
+        await db.collection("messages").insertOne( { from: name, to: 'Todos', text: 'sai da sala...', type: 'status', time: time });
+            
+        res.status(201).send('Usuario saiu da sala com sucesso!');
+    }  catch (error) {
+        res.status(500).send('Nao foi possivel deletar o usuario.')
+    }
+       
+});}, 15000);
 
 
 app.post('/messages', (req, res) => {
